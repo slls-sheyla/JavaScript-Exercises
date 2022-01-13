bb41ad182c96c19ab6b402f18e2a6d3bacf72fa7
Sheyla Lima <slls.sheyla@gmail.com>
2022-01-13 13:20:05 -0300

    Callback

---
+++ funcao/callback2.js
@@ -0,0 +1,25 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem callback
+let notasBaixas = []
+for (let i in notas) {
+    if(notas[i] < 7) {
+        notasBaixas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+
+// Com callback
+notasBaixas1 = notas.filter(function(nota) {
+    return nota < 7
+})
+
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notas.filter(notasMenorQue7)
+console.log(notasBaixas3)
+
+console.log(notasBaixas1)
+
+const notasBaixas2 = notas.filter(nota => nota < 7)
+console.log(notasBaixas2)
