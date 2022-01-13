5cd5ab0cb131e23a96925876eb7e7aed7e0cae7c
Sheyla Lima <slls.sheyla@gmail.com>
2022-01-13 12:48:04 -0300

    Funcao arrow

--- funcao/arrowFunction3.js
+++ funcao/arrowFunction3.js
@@ -3,3 +3,12 @@ let comparaComThis = function (param) {
 }
 
 comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
